aa8d5a8fb679af533a18d2b3fd897f4a511e79ac
Yen Lin <e19970722@gmail.com>
2024-11-05 21:52:14 +0800

    add db

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const app = express();
+var sqlite3 = require('sqlite3').verbose();
 const PORT = 4000;
 
+var db = new sqlite3.Database('./MoneyNote.db', (err) => {
+  if (err) {
+    console.error('Error opening database: ' + err.message);
+  } else {
+    console.log('Connected to the MoneyNote SQLite database.');
+  }
+});
+
+function transformData(inputArray) {
+  return {
+    records: inputArray.map(item => ({
+      fields: {
+        category: item.category,
+        note: item.note,
+        date: item.date,
+        amount: item.amount,
+        expenseIncome: item.expenseIncome
+      }
+    }))
+  };
+}
+
 app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
+  db.all('SELECT * FROM MoneyNoteTable', [], (err, rows) => {
+    if (err) {
+        console.error(err.message);
+        res.status(500).send("Error fetching MoneyNoteTable");
+    } else {       
+        var formatData = transformData(rows);     
+        res.send(formatData);
+    }
+  });
 });
 
 app.listen(PORT, () => {
